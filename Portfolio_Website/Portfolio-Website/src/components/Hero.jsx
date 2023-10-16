11060595066fa074290fca6178c77216f5568a91
lwalker <landen@example.com>
2023-10-15 23:16:03 -0500

    added 3d render of desktop to website (still need to fix the lighting on it)

--- Portfolio_Website/Portfolio-Website/src/components/Hero.jsx
+++ Portfolio_Website/Portfolio-Website/src/components/Hero.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import { motion } from 'framer-motion';
+import { styles } from '../style';
+import { ComputersCanvas } from './canvas';
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full
+    h-screen mx-auto">
+      <div className={`${styles.paddingX} 
+      absolute inset-0 top-[120px] max-w-7x1 mx-auto flex 
+      flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center
+         items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText}
+          text-white`}>Hi, my name is <span 
+          className="text-[#915eff]">Landen
+          </span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am a fullstack web developer <br 
+            className="sm:block hidden" /> with a drive to push myself
+            and learn all that I can.
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+    </section>
   )
 }
 
